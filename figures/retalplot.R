be20870021d37a1ac11d35a57df7d83d0d262006
George G. Vega Yon <g.vegayon@gmail.com>
2018-03-10 17:26:05 -0800

    Now I like this

--- figures/retalplot.R
+++ figures/retalplot.R
@@ -7,13 +7,13 @@ dat <- readr::read_csv("data/TEST_OPED_MC_WASHINGTONPOST.csv")
 fancy_plot <- function(x, ctr = "United States of America", cols = c("gray", "tomato")) {
   
   # Generating ids -------------------------------------------------------------
-  ids <- sort(unique(with(x, c(ctry1_rep, ctry2_par)))) %>%
+  ids <- sort(unique(with(x, c(ctry1_rep, ctry2_par))), decreasing = TRUE) %>%
     setdiff(ctr)
   
   ids <- tibble::tibble(
     id   = 1L:length(ids),
     name = ids
-  )
+  ) 
   
   # Setting us in the middle 
   ctr_id <- as.integer(floor(nrow(ids)/2))
@@ -47,80 +47,77 @@ fancy_plot <- function(x, ctr = "United States of America", cols = c("gray", "to
   # Creating the plot ----------------------------------------------------------
   oldpar <- graphics::par(no.readonly = TRUE)
   on.exit(graphics::par(oldpar))
-  par(las = 2, mai = oldpar$mai*c(1,1,0,1))
+  par(las = 2, mai = oldpar$mai*c(1,1.5,0,1))
   plot.new()
   plot.window(xlim = range(years), ylim = range(ids$id))
   
+  # Maxi height
+  MAX_H <- nrow(ids)/10
+  
   # Wrapper of xspline
-  myxspline <- function(y, from, to, qty, curve = c(.2, .8)) {
+  myxspline <- function(year, from, to, qty_from, qty_to, col, curve = c(.2, .8)) {
     
-    coli <- colfun(qty)
-    coli <- grDevices::rgb(
-      coli[1], coli[2], coli[3],
-      maxColorValue = 255,
-      alpha = 70
-      )
+    # coli <- colfun(qty_to)
+    # coli <- grDevices::rgb(
+    #   coli[1], coli[2], coli[3],
+    #   maxColorValue = 255,
+    #   alpha = 70
+    #   )
       
-    qty <- qty*20
+    qty_from <- qty_from*MAX_H
+    qty_to   <- qty_to*MAX_H
+    
+    if (!qty_from & qty_to)
+      return(NULL)
+    
     xspline(
       x     = c(
-        y,
-        y + .25,
-        y + .5,
-        y + .5,
-        y + .25,
-        y),
+        year,
+        year + .5,
+        year + 1,
+        year + 1,
+        year + .5,
+        year),
       y     = c(
-        from + qty/2,
-        from*curve[1] + (to + qty/2)*curve[2],
-        to + qty/2,
-        to - qty/2, from*curve[1] + (to - qty/2)*curve[2],
-        from - qty/2
+        from + qty_from/2,
+        (from + qty_from/2)*curve[1] + (to + qty_to/2)*curve[2],
+        to + qty_to/2,
+        to - qty_to/2, (from - qty_from/2)*curve[1] + (to - qty_to/2)*curve[2],
+        from - qty_from/2
         ),
-      col    = coli,
-      shape  = -.5, #c(-.7, -.7, 0, .7, .7),
+      col    = grDevices::adjustcolor(col, 1-abs(qty_to - qty_from)/1.25),
+      shape  = c(0, -.7, 0, 0, .7, 0),
       border = NA,
       open   = FALSE
       # lwd    = (qty^(1.5)*max_received)^(1/1.5)
       )
   }
   
+  for (i in ids$id)
+    abline(h = i, col = adjustcolor(i, .3), lwd=1, lty=2)
+  
   # From US to the world
   for (y in years[-length(years)]) {
   
     # Receiving countries
-    z <- x %>%
+    z_from <- x %>%
       filter(yr_impl == y & ctry1_rep == ctr) %>%
       group_by(ctry2_par) %>%
       summarize(
-        received = sum(protec1)
+        sent = sum(protec1)
       ) %>%
-      filter(received > 0) %>%
+      filter(sent > 0) %>%
       rename(name = ctry2_par) %>%
       dplyr::left_join(ids, by = "name") %>%
-      mutate(
-        received = coalesce(received, 0L)
+      transmute(
+        country = id,
+        from    = coalesce(sent, 0L)/max_received
       )
       
-    # Is there any data?
-    if (nrow(z) == 0)
-      next
-    
-    # Rescaling
-    z$received <- with(z, received/max_received)
-    
-    for (i in 1:nrow(z)) {
-      myxspline(y = y - .5, from = ctr_id, to = z$id[i], qty = z$received[i])
-    }
-  
-  }
-  
-  # From the world to US 
-  for (y in years[-length(years)]) {
     
     # Receiving countries
-    z <- x %>%
-      filter(yr_impl == y & ctry2_par == ctr) %>%
+    z_to <- x %>%
+      filter(yr_impl == (y + 1L) & ctry2_par == ctr) %>%
       group_by(ctry1_rep) %>%
       summarize(
         received = sum(protec1)
@@ -128,29 +125,43 @@ fancy_plot <- function(x, ctr = "United States of America", cols = c("gray", "to
       filter(received > 0) %>%
       rename(name = ctry1_rep) %>%
       dplyr::left_join(ids, by = "name") %>%
-      mutate(
-        received = coalesce(received, 0L)
+      transmute(
+        country  = id,
+        to       = coalesce(received, 0L)/max_received
       )
     
     # Is there any data?
-    if (nrow(z) == 0)
+    if (nrow(z_to) == 0)
       next
     
-    # Rescaling
-    z$received <- with(z, received/max_received)
+    # merging
+    d <- full_join(z_from, z_to, by = "country") %>%
+      mutate(
+        from = coalesce(from, 0),
+        to   = coalesce(to, 0)
+      )
     
-    for (i in 1:nrow(z)) {
-      myxspline(y = y, from = z$id[i], to = ctr_id, qty = z$received[i], curve = c(.8, .2))
+    for (i in 1:nrow(d)) {
+      # myxspline(y = y, from = z$id[i], to = ctr_id, qty = z$received[i], curve = c(.8, .2))
+      myxspline(
+        year = y,
+        from = d$country[i],
+        to   = d$country[i],
+        qty_from = d$from[i],
+        qty_to   = d$to[i],
+        curve = c(.8, .2),
+        col = d$country[i]
+        )
     }
     
   }
   
   axis(1, labels = years, at = years)
-  axis(2, labels = ids$name, at = ids$id, cex.axis=.2, tick = FALSE)
+  axis(2, labels = ids$name, at = ids$id, cex.axis=.5, tick = FALSE)
   
   
 }
 
 dat %>% 
-  filter(yr_impl >= 2014, yr_impl <= 2018, protec1 >= 20) %>%
+  filter(yr_impl >= 2014, yr_impl <= 2017, protec1 >= 20) %>%
   fancy_plot
